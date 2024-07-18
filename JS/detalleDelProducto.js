6db9af9b2fc4539df626245bc004b21c08a372ff
unknown <mateox463@gmail.com>
2024-07-18 11:42:24 -0300

    se actualiza detalles de productos

--- JS/detalleDelProducto.js
+++ JS/detalleDelProducto.js
@@ -11,7 +11,7 @@ const productoFiltrado = productosLocalStorage.find(
 );
 
 divDetalleProducto.innerHTML = `
-  <div>
+  <div d-flex>
     <div class='w-25 justify-content-end'>
       <img src="${productoFiltrado.imagen}" alt="" class='w-100'/>
     </div>
@@ -23,3 +23,84 @@ divDetalleProducto.innerHTML = `
     <button class="btn btn-success" id='idBotonCarrito'>Añadir al Carrito</button>
   </div>
 `;
+
+const botonFavoritos = document.querySelector("#idBotonFavoritos");
+const botonCarrito = document.querySelector("#idBotonCarrito");
+
+const agregarProductoAFavoritos = () => {
+  const producto = productos.find((prod) => prod.id === idProducto);
+  const usuarioSesionStorage =
+    JSON.parse(sessionStorage.getItem("usuario")) || "";
+  const usuariosLocalStorage =
+    JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  if (!usuarioSesionStorage) {
+    alert("Debes iniciar sesión.");
+    setTimeout(() => {
+      location.href = "../pages/iniciar-sesion.html";
+    }, 1000);
+    return;
+  }
+
+  if (usuarioSesionStorage.favoritos.length) {
+    const productoFiltrado = usuarioSesionStorage.favoritos.find(
+      (prod) => prod.id === producto.id
+    );
+    if (productoFiltrado) {
+      return alert("Producto ya existe en favoritos");
+    }
+  }
+
+  usuarioSesionStorage.favoritos.push(producto);
+
+  const posicionDelUsuarioEnElArray = usuariosLocalStorage.findIndex(
+    (usuario) => usuario.id === usuarioSesionStorage.id
+  );
+
+  usuariosLocalStorage[posicionDelUsuarioEnElArray] = usuarioSesionStorage;
+
+  localStorage.setItem("usuarios", JSON.stringify(usuariosLocalStorage));
+  sessionStorage.setItem("usuario", JSON.stringify(usuarioSesionStorage));
+  alert("Producto cargado en favoritos");
+};
+
+const agregarProductoACarrito = () => {
+  const producto = productos.find((prod) => prod.id === idProducto);
+  const usuarioSesionStorage =
+    JSON.parse(sessionStorage.getItem("usuario")) || "";
+  const usuariosLocalStorage =
+    JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  if (!usuarioSesionStorage) {
+    alert("Debes iniciar sesión.");
+    setTimeout(() => {
+      location.href = "../pages/iniciar-sesion.html";
+    }, 1000);
+    return;
+  }
+
+  if (usuarioSesionStorage.carrito.length) {
+    const productoFiltrado = usuarioSesionStorage.carrito.find(
+      (prod) => prod.id === producto.id
+    );
+    if (productoFiltrado) {
+      return alert("Producto ya existe en el carrito");
+    }
+  }
+
+  usuarioSesionStorage.carrito.push(producto);
+
+  const posicionDelUsuarioEnElArray = usuariosLocalStorage.findIndex(
+    (usuario) => usuario.id === usuarioSesionStorage.id
+  );
+
+  usuariosLocalStorage[posicionDelUsuarioEnElArray] = usuarioSesionStorage;
+
+  localStorage.setItem("usuarios", JSON.stringify(usuariosLocalStorage));
+  sessionStorage.setItem("usuario", JSON.stringify(usuarioSesionStorage));
+  alert("Producto cargado en el carrito");
+};
+
+// Asignar el evento click a los botones
+botonFavoritos.addEventListener("click", agregarProductoAFavoritos);
+botonCarrito.addEventListener("click", agregarProductoACarrito);
--- JS/home-usuario.js
+++ JS/home-usuario.js
@@ -6,12 +6,17 @@
 //   divMensajeBienvenida.innerHTML = `<h3>Bienvenido ${usuarioSessionStorage.nombreDeUsuario}</h3>`;
 // }, 1000);
 
+const usuarioSessionStorage =
+  JSON.parse(sessionStorage.getItem("usuarios")) || [];
+
 const productosLocalStorage =
   JSON.parse(localStorage.getItem("productos")) || [];
-const divCards = document.getElementById("idDivcards");
+const divCards = document.getElementById("idDivCards");
+const cerrarSesionUsuario = document.getElementById("idCerrarSesionUsuario");
 
-divCards.innerHTML = productosLocalStorage.map(
-  (producto) => `
+divCards.innerHTML = productosLocalStorage
+  .map(
+    (producto) => `
   <div class="col-12 col-md-6 col-lg-4">
                   <div class="card colorCard">
                     <img
@@ -26,9 +31,28 @@ divCards.innerHTML = productosLocalStorage.map(
                       </p><p class="card-text">
                         ${producto.descripcion}
                       </p>
-                      <a href="./pages/detallesProductos.html?id=${producto.id}" class="btn btn-primary">Ver Mas</a>
+                      <a href="../pages/detallesProductos.html?id=${producto.id}" class="btn btn-primary">Ver Mas</a>
                     </div>
                   </div>
                 </div>
 `
-);
+  )
+  .join("");
+
+const cerrarSesion = () => {
+  const usuariosLocalStorage =
+    JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  const posicionDelUsuarioEnElArray = usuariosLocalStorage.findIndex(
+    (usuario) => usuario.id === usuarioSessionStorage.id
+  );
+
+  usuariosLocalStorage[posicionDelUsuarioEnElArray].login = false;
+  sessionStorage.removeItem("usuario");
+  localStorage.setItem("usuarios", JSON.stringify(usuariosLocalStorage));
+
+  setTimeout(() => {
+    location.href = "../index.html";
+  }, 3000);
+};
+cerrarSesionUsuario.addEventListener("click", cerrarSesion);
