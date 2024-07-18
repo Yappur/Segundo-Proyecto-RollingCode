eec926e4890e83115039c97f4a64dd9efeeae9a7
unknown <mateox463@gmail.com>
2024-07-17 23:13:43 -0300

    se crea detalles de productos y fix en navbar

---
+++ JS/home-usuario.js
@@ -0,0 +1,7 @@
+// const divMensajeBienvenida = document.getElementById("bienvenidaUsuario");
+// const usuarioSessionStorage =
+//   JSON.parse(sessionStorage.getItem("usuario")) || "";
+
+// setTimout(() => {
+//   divMensajeBienvenida.innerHTML = `<h3>Bienvenido ${usuarioSessionStorage.nombreDeUsuario}</h3>`;
+// }, 1000);
